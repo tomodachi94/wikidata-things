3a628b40f15ca96e137540bb2394efba7018343a
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-05-01 21:00:38 -0700

    Make `parse_wiki_dump_to_get_curseforge_urls` function more generic
    This will aid with obtaining values from other services or other wikis.

--- scripts/scrape-curseforge/parse_wiki_dump_to_get_curseforge_urls.py
+++ scripts/scrape-curseforge/parse_wiki_dump_to_get_curseforge_urls.py
@@ -1,12 +1,19 @@
 from bs4 import BeautifulSoup
 
-from tomodb import Database # local
+from tomodb import Database  # local
 
-db = Database("./urls.sqlite3")
 
-def parse_curseforge_urls(xml_file):
-    with open(xml_file, "r", encoding="utf-8") as f:
-        soup = BeautifulSoup(f, "xml")
+def parse_curseforge_urls(
+    xml_file,
+    regex=r"\b(https|http)?://(?:(www|minecraft)\.)?curseforge\.com\/minecraft\S+\b",
+    field_name="curseforge_url",
+):
+    """A function which parses a MediaWiki export for CurseForge URLs.
+
+    This function's parameters can be replaced with other values to scrape
+    other services from the wiki; look at the `regex` and `field_name` params.
+    """
+    soup = BeautifulSoup(xml_file, "xml")
 
     results = []
     for page in soup.find_all("page"):
@@ -15,19 +22,24 @@ def parse_curseforge_urls(xml_file):
 
         # Extract URLs using regex
         import re
-        urls = re.findall(r"\b(https|http)?://(?:(www|minecraft)\.)?curseforge\.com\/minecraft\S+\b", content)
+
+        urls = re.findall(regex, content)
 
         for url in urls:
-            results.append({"title": title, "curseforge_url": url})
+            results.append({"title": title, field_name: url})
 
     return results
 
-urls = parse_curseforge_urls("./dump.xml")
 
-for item in urls:
-    if not db.get(item['curseforge_url']):
-        db.store(item)
-    else:
-        print(f"ERR: {item['title']} has duplicate")
+def store_curseforge_urls():
+    db = Database("./urls.sqlite3")
+    with open("./dumps.xml", encoding="utf-8") as xml_file:
+        urls = parse_curseforge_urls(xml_file)
 
-db.close()
+    for item in urls:
+        if not db.get(item["curseforge_url"]):
+            db.store(item)
+        else:
+            print(f"ERR: {item['title']} has duplicate")
+
+    db.close()
